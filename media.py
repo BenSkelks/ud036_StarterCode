070ad1d2321f27880b0b1e1aa3b8e7abea3e9aa0
Ben Skelker <bskelker@cisco.com>
2018-03-18 11:35:01 +0200

    feature: Add movie parental guidance to website
    Started

--- media.py
+++ media.py
@@ -4,13 +4,17 @@ import webbrowser
 class Movie():
     """Class for storing movie information"""
 
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    VALID_PARENTAL_GUIDANCE = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, parental_guidance=None):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        if parental_guidance is None:
+            parental_guidance = "NOT FIT FOR HUMAN CONSUMPTION"
+        self.parental_guidance= parental_guidance
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
